08e04ba459741b41a151d517636e036ba3d90513
99002538 <71540713+99002538@users.noreply.github.com>
2020-11-11 15:13:02 +0530

    Update server.js
    Added server with listening port

--- 3_Implementation/server.js
+++ 3_Implementation/server.js
@@ -24,3 +24,8 @@ app.use('/items', itemsRouter);
 app.use('/', function(req, res) {
     res.send('node-ex-api works :-)');
 });
+
+const server = http.createServer(app);
+const port = 3000;
+server.listen(port);
+console.debug('Server listening on port ' + port);
